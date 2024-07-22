59f49220fff94039cc9472c30df70cf85543d863
elif <elifnurilgaz@hotmail.com>
2024-07-22 18:44:29 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import IlanList from './components/IlanList'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <h1>İş İlanı Portalı</h1>
+      <div className='header'>
+        <div className='arama_kutusu'>
+          <input type='text' placeholder='İş İlanı Arayın'></input>
+          <button id='arama_butonu'>Ara</button>
+
+        </div>
+
+      </div>
+      <IlanList/>
+
+    </div>
+  )
+}
+
+export default App
